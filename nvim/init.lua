0e9e3c7a27e8466ed5d067ac3f696c3907fbef7d
puppuccino <57177806+puppuccino@users.noreply.github.com>
2023-09-08 16:16:13 +0000

    update init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,43 @@
+local opt = vim.opt
+local keymap = vim.keymap
+opt.shell = 'zsh'
+
+-- Options --
+opt.number = true
+opt.backup = false
+opt.swapfile = false
+opt.smartindent = true
+opt.showmatch = true
+opt.matchtime = 1
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.wrapscan = true
+opt.virtualedit = "onemore"
+-- opt.whichwrap
+
+-- keymaps --
+local noresil = { noremap = true, silent = true }
+keymap.set('i', 'jj', '<esc>', noresil)
+keymap.set('n', 'j', 'gj', noresil)
+keymap.set('n', 'k', 'gk', noresil)
+keymap.set('n', '+', '<C-a>', noresil)
+keymap.set('n', '-', '<C-x>', noresil)
+keymap.set('n', 'Y', 'y$', noresil)
+-- type semicolon to enter the command mode
+keymap.set('n', ';', ':', { noremap = true })
+-- move to head or tail of current line
+vim.g.mapleader = ' '
+keymap.set('n', '<leader>a', '^', noresil)
+keymap.set('v', '<leader>a', '^', noresil)
+keymap.set('n', '<leader>f', '$', noresil)
+keymap.set('v', '<leader>f', '$', noresil)
+-- type z+Enter to move current line to the top of screen, keeping cursor position
+keymap.set('n', 'z<CR>', 'zt', noresil)
+-- turn off highlight
+keymap.set('n', '<leader><leader>', ':nohl<CR>', noresil)
+-- exit terminal insert mode
+keymap.set('t', '<esc>', '<C-\\><C-n>', noresil)
